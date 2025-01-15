6af79df59ef4edc4d94dd93e9eaf1959dcfade84
Jasim Uddin <jasimbdpro@gmail.com>
2025-01-15 22:14:48 +0600

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = withPWA( {
+    dest: "public", // This is where the service worker and other assets will be generated
+    register: true, // Automatically registers the service worker
+    skipWaiting: true, // Skip waiting on the new service worker
+  });
 
 export default nextConfig;
