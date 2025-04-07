54d70e5c53f46c18a7b5307e112c6ea5d84eccae
unknown <jasimbdpro@gmail.com>
2025-04-07 22:19:14 +0600

    .

--- lib/database.ts
+++ lib/database.ts
@@ -1,26 +1,19 @@
 import mongoose from "mongoose";
-
 let isConnected = false;
-
 export const connectToDb = async (): Promise<void> => {
     mongoose.set("strictQuery", true);
-
     if (isConnected) {
         console.log("MongoDB is already connected");
         return;
     }
-
     try {
         const mongoUri = process.env.MONGODB_URI;
-
         if (!mongoUri) {
             throw new Error("MONGODB_URI is not defined in environment variables.");
         }
-
         await mongoose.connect(mongoUri, {
             dbName: "recent_job",
         });
-
         isConnected = true;
         console.log("MongoDB is connected");
     } catch (error) {
